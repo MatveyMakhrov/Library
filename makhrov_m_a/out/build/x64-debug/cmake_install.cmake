d6e5f86eae76a63ef69b4c801c279b84f657286c
MatveyMakhrov <113934714+MatveyMakhrov@users.noreply.github.com>
2023-03-27 21:45:48 +0300

    add rational library

--- makhrov_m_a/out/build/x64-debug/cmake_install.cmake
+++ makhrov_m_a/out/build/x64-debug/cmake_install.cmake
@@ -33,9 +33,13 @@ if(NOT DEFINED CMAKE_CROSSCOMPILING)
 endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
-  # Include the install script for each subdirectory.
-  include("C:/z/oop/makhrov_m_a/out/build/x64-debug/makhrov_m_a/cmake_install.cmake")
+  # Include the install script for the subdirectory.
+  include("C:/z/oop/makhrov_m_a/out/build/x64-debug/prj.lab/cmake_install.cmake")
+endif()
 
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for the subdirectory.
+  include("C:/z/oop/makhrov_m_a/out/build/x64-debug/prj.test/cmake_install.cmake")
 endif()
 
 if(CMAKE_INSTALL_COMPONENT)
